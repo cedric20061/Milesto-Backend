30dfb3ede82aa355b55b6d281d66a57ec7fc5327
Gérald <cedricguidi735@gmail.com>
2024-11-22 22:20:30 +0100

    Fix dist paths and Vercel config

--- main.ts
+++ main.ts
@@ -1,14 +1,13 @@
-import 'module-alias/register';
+import "module-alias/register";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import connectDB from "./config/db.js";
+import connectDB from "@config/db.js";
 import authRoutes from "@routes/authRoutes";
 import goalsRoutes from "@routes/goalsRoutes";
 import dailySchedule from "@routes/dailyScheduleRoutes";
 import userRoutes from "@routes/userRoutes";
 import cors from "cors";
-
 
 dotenv.config();
 connectDB();
@@ -33,6 +32,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to the API!");
 });
 const PORT = process.env.PORT || 5000;
-//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;

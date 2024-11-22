9040d4f1427f6514a10378360d9d1e8e0ad3fc3c
Gérald <cedricguidi735@gmail.com>
2024-11-22 21:57:51 +0100

    V1

--- main.ts
+++ main.ts
@@ -33,6 +33,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to the API!");
 });
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;

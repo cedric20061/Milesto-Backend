95accfc814acc9f971e159d91f2f81bca48d91df
Gérald <cedricguidi735@gmail.com>
2024-11-23 15:11:30 +0100

    Fix dist paths and Vercel config

--- main.ts
+++ main.ts
@@ -38,7 +38,17 @@ app.use(
     credentials: true,
   })
 );
-
+app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.ALLOWED_ORIGIN || "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("Request Origin:", req.headers.origin);
+  next();
+});
 app.use(authRoutes);
 app.use(goalsRoutes);
 app.use(dailySchedule);

d05bc4da0c8a1f346962e6c1bc2e785e871f0ea0
Gérald <cedricguidi735@gmail.com>
2024-11-23 12:04:54 +0100

    Fix dist paths and Vercel config

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,9 +4,11 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
+    mongoose.set("debug", true);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
+    mongoose.set("debug", true);
     console.error(error.message);
     process.exit(1);
   }

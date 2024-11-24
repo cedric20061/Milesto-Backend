cbeb6834bf7b974b9b5fa007d36d0b09a3e75de8
Gérald <cedricguidi735@gmail.com>
2024-11-24 10:32:39 +0100

    mongoose fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,11 +4,9 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    mongoose.set("debug", true);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
-    mongoose.set("debug", true);
     console.error(error.message);
     process.exit(1);
   }

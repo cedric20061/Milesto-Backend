557cd633fd2a00e9cc1dda7129ac0260b352a775
Gérald <cedricguidi735@gmail.com>
2025-10-08 23:02:41 +0100

    feat: error fix

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -2,7 +2,7 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { Response, NextFunction } from "express";
 import User from "@models/user.js";
 import { CustomRequest } from "@types";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -16,8 +16,7 @@ export const protect = async (
   res: Response,
   next: NextFunction
 ) => {
-  const token =
-  req.cookies.token || req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
   if (!token) {
     res.status(401).json({ message: "Not authorized, no token" });
     return;
